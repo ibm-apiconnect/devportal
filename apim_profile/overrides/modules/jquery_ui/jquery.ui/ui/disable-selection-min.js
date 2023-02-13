0832046779f46df6d4eba33ece15b9afe2575ea2
slnode <slnode@ca.ibm.com>
2023-02-13 17:42:12 +0000

    APIC v2018.4.1.24

---
+++ apim_profile/overrides/modules/jquery_ui/jquery.ui/ui/disable-selection-min.js
@@ -0,0 +1,4 @@
+/*! jQuery UI - v1.13.2 - 2022-09-11
+* http://jqueryui.com
+* Copyright jQuery Foundation and other contributors; Licensed  */
+!function(e){"use strict";"function"==typeof define&&define.amd?define(["jquery","./version"],e):e(jQuery)}((function(e){"use strict";return e.fn.extend({disableSelection:(n="onselectstart"in document.createElement("div")?"selectstart":"mousedown",function(){return this.on(n+".ui-disableSelection",(function(e){e.preventDefault()}))}),enableSelection:function(){return this.off(".ui-disableSelection")}});var n}));
---
+++ apim_profile/overrides/modules/jquery_ui/jquery.ui/ui/safe-active-element-min.js
@@ -0,0 +1,4 @@
+/*! jQuery UI - v1.13.2 - 2022-09-11
+* http://jqueryui.com
+* Copyright jQuery Foundation and other contributors; Licensed  */
+!function(e){"use strict";"function"==typeof define&&define.amd?define(["jquery","./version"],e):e(jQuery)}((function(e){"use strict";return e.ui.safeActiveElement=function(e){var n;try{n=e.activeElement}catch(t){n=e.body}return n||(n=e.body),n.nodeName||(n=e.body),n}}));
