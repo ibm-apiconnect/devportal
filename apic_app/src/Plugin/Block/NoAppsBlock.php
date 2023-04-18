3cef67240c101d469510f82ef6d94c3b351da208
andrew farquharson <farquharson.a@gmail.com>
2023-04-18 23:11:56 +0100

    create custom block, template in apic_app module

---
+++ apic_app/src/Plugin/Block/NoAppsBlock.php
@@ -0,0 +1,80 @@
+<?php
+
+/********************************************************* {COPYRIGHT-TOP} ***
+ * Licensed Materials - Property of IBM
+ * 5725-L30, 5725-Z22
+ *
+ * (C) Copyright IBM Corporation 2018, 2022
+ *
+ * All Rights Reserved.
+ * US Government Users Restricted Rights - Use, duplication or disclosure
+ * restricted by GSA ADP Schedule Contract with IBM Corp.
+ ********************************************************** {COPYRIGHT-END} **/
+
+/**
+ * @file
+ * Contains \Drupal\apic_app\Plugin\Block\NoAppsBlock.
+ */
+
+namespace Drupal\apic_app\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Cache\UncacheableDependencyTrait;
+use Drupal\Core\Link;
+use Drupal\Core\Routing\CurrentRouteMatch;
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\Core\Url;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+
+
+/**
+ * Provides a block to display when the applications view page display has no results.
+ *
+ * @Block(
+ *   id = "noappsblock",
+ *   admin_label = @Translation("No Apps Block"),
+ *   category = @Translation("IBM API Developer Portal (Application)")
+ * )
+ *
+ */
+class NoAppsBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * NoAppsBlock constructor.
+   *
+   * @param array $configuration
+   * @param $plugin_id
+   * @param $plugin_definition
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+  /**
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   * @param array $configuration
+   * @param string $plugin_id
+   * @param mixed $plugin_definition
+   *
+   * @return \Drupal\apic_app\Plugin\Block\NoAppsBlock|static
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): NoAppsBlock {
+    return new static($configuration,
+      $plugin_id,
+      $plugin_definition,
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build(): array {
+
+    return [
+      '#theme' => 'no_apps'
+    ];
+  }
+
+}
