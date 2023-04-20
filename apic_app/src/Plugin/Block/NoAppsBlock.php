a5d8a0559a7126d492b34c626313da983515aafd
andrew farquharson <farquharson.a@gmail.com>
2023-04-20 21:19:17 +0100

    remove unneeded constructor from block class

--- apic_app/src/Plugin/Block/NoAppsBlock.php
+++ apic_app/src/Plugin/Block/NoAppsBlock.php
@@ -19,15 +19,6 @@
 namespace Drupal\apic_app\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
-use Drupal\Core\Cache\UncacheableDependencyTrait;
-use Drupal\Core\Link;
-use Drupal\Core\Routing\CurrentRouteMatch;
-use Drupal\Core\Session\AccountProxyInterface;
-use Drupal\Core\Url;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-
-
 
 /**
  * Provides a block to display when the applications view page display has no results.
@@ -40,32 +31,6 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *
  */
 class NoAppsBlock extends BlockBase implements ContainerFactoryPluginInterface {
-
-  /**
-   * NoAppsBlock constructor.
-   *
-   * @param array $configuration
-   * @param $plugin_id
-   * @param $plugin_definition
-   */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-  }
-
-  /**
-   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
-   * @param array $configuration
-   * @param string $plugin_id
-   * @param mixed $plugin_definition
-   *
-   * @return \Drupal\apic_app\Plugin\Block\NoAppsBlock|static
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition): NoAppsBlock {
-    return new static($configuration,
-      $plugin_id,
-      $plugin_definition,
-    );
-  }
 
   /**
    * {@inheritdoc}
